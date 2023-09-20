de488ba526a45bdb44e39911fea53a60ea6b59d1
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-09-20 22:36:06 +0300

    added user registration

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import axios from "axios";
 export default function Register(){
 
+const [error,setError] = useState(false);
+const [details,setDetails] = useState(
+    {
+        username:'',
+        email:'',
+        password: ''
+        }
+);
+
+  const handleChange = (e)=>{
+    const {name,value} = e.target;
+    setDetails((prevDetails)=> ({
+        ...prevDetails,
+        [name] :value
+    })
+
+    )
+  };
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try{
+        const res = await axios.post('http://127.0.0.1:3000/api/users',
+            {
+                username: details.username,
+                email:details.email,
+                password:details.password
+
+            }
+        );
+        console.log(details);
+          res.data && window.location.replace("/login");
+    }catch(err){
+        setError(true);
+        console.log("details are");
+        console.log(details);
+        console.log(err);
+    };
+  };
     async function test(){
         try{
             const res = await axios.get('http://127.0.0.1:3000/api/users');
-            console.log(res);
+            //console.log(res);
         }catch(err){
             console.log(err);
         };
@@ -16,21 +54,37 @@ export default function Register(){
     return(
         <>
         <h2>Register as a new user</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
 
             <div>
                 <label htmlFor="username">Enter username</label>
-                <input type="text" />
+                <input 
+                type="text"
+                name = 'username'
+                placeholder='unique username'
+                value = {details.username}
+                onChange={(e)=>handleChange(e)}
+                 />
             </div>
 
             <div>
                 <label htmlFor="email">Enter email address</label>
-                <input type="email" />
+                <input type="email" 
+                    name = 'email'
+                    placeholder='email address'
+                    value = {details.email}
+                    onChange={(e)=>handleChange(e)}
+                />
             </div>
 
             <div>
-                <label htmlFor="password">password</label>
-                <input type="password" />
+                <label htmlFor="password">Enter password</label>
+                <input type="password" 
+                name = 'password'
+                placeholder='password'
+                value = {details.password}
+                onChange={(e)=>handleChange(e)}
+                />
             </div>
             
             <button type='submit'>REGISTER</button>
