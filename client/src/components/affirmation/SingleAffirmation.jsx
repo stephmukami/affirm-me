723ec5331fc46c7c6af76f32e7c2c29b6a400e9e
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-09-25 20:48:39 +0300

    added delete function

--- client/src/components/affirmation/SingleAffirmation.jsx
+++ client/src/components/affirmation/SingleAffirmation.jsx
@@ -9,10 +9,12 @@ function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
   const [editedAffirmation, setEditedAffirmation] = useState(affirmation);
   const { userValue } = useContext(UserContext);
 
+  //changing to text box mode
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
+  //submit updated post
   const handleSaveClick = () => {
     // Get the bearer token from your context
     const bearerToken = userValue.token;
@@ -38,6 +40,33 @@ function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
         console.error('Error updating affirmation:', error);
       });
   };
+
+  //delete affirmation
+  const handleDelete = () => {
+    // Get the bearer token from your context
+    const bearerToken = userValue.token;
+  
+    // Set up Axios with the authorization header
+    const axiosInstance = axios.create({
+      baseURL: 'http://127.0.0.1:3000', // Replace with your API URL
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${bearerToken}` // Include the bearer token
+      }
+    });
+  
+    // Make the PUT request using the configured Axios instance
+    axiosInstance.delete(`/api/affirmations/${id}`)
+      .then(response => {
+        console.log('Affirmation deleted:', response.data);
+       //refresh page 
+       window.location.reload();
+      })
+      .catch(error => {
+        console.error('Error delete affirmation:', error);
+      });
+  };
+
   
   return (
     <>
@@ -58,7 +87,7 @@ function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
           {isEditing ? (
             <button onClick={handleSaveClick}>Save</button>
           ) : (
-            <svg
+            <svg className='edit-icon'
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 24 24'
               onClick={handleEditClick}
@@ -67,7 +96,10 @@ function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
               <path d='M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z' />
             </svg>
           )}
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>
+          <svg 
+          className='delete-icon'
+          onClick={handleDelete}
+           xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>
           <button>next</button>
         </div>
       </div>
