44dd0705c355546fabbeb7d662d6530da4512f5d
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-10-06 23:22:47 +0300

    added clode sidebar functionality

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './sidebar.css'
-export default function Sidebar() {
+export default function Sidebar({ toggleSidebar }) {
 
   const handleLogout = ()=>{
   localStorage.clear();
     window.location.href = '/';
+  };
+
+  const handleSidebarToggle = () => {
+    console.log(" sidebar sth happens");
+
+    toggleSidebar();
   };
 
   return (
@@ -24,7 +30,7 @@ export default function Sidebar() {
             <h4  onClick={handleLogout}>Log out</h4>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>logout</title><path d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z" /></svg>
         </div>
-        <h4>Close sidebar</h4>
+        <h4 onClick={handleSidebarToggle}>Close sidebar</h4>
       </div>
     </>
     
--- client/src/pages/home-page/Home.jsx
+++ client/src/pages/home-page/Home.jsx
@@ -1,21 +1,30 @@
-import React ,{useContext}from 'react';
+import React ,{useContext,useState}from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import { UserContext } from '../../Context/UserContext';
 import AffirmationSection from '../../components/affirmation/AffirmationSection';
 import './home.css' 
-export default function Home(){
+export default function Home() {
     const {userValue,setUserValue} = useContext(UserContext);
+    const [viewSidebar,setViewSidebar] = useState(true);
+
+const toggleSidebar = () => {
+     setViewSidebar(prevViewSidebar => !prevViewSidebar);
+      ;
+}
+const sidebarVar =   <Sidebar toggleSidebar={toggleSidebar} viewSidebar = {viewSidebar}/>
+const reveal =  <h3 onClick = {toggleSidebar}>Open sidebar</h3>
+ 
     return (
         <UserContext.Provider value={{userValue,setUserValue}}>
             <div>
                 <Navbar/>
                 <div className="home-page">
-                <Sidebar/>
+                {viewSidebar ? sidebarVar:reveal}
                  <AffirmationSection/>
                 </div>
             </div>
         </UserContext.Provider>
         
     )
- }
+    }
