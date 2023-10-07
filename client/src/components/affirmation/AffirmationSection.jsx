5040c3bbd33b6f9dfcdca9013e901d043cb72284
stephmukami33 <mukami.kariuki1@students.jkuat.ac.ke>
2023-10-07 22:04:40 +0300

    added randomly selectable affirmations, deleted unused files

--- client/src/components/affirmation/AffirmationSection.jsx
+++ client/src/components/affirmation/AffirmationSection.jsx
@@ -1,48 +1,62 @@
-import React, {useEffect,useState,useContext} from 'react'
-import './affirmationSection.css'
-import axios from'axios';
-import SingleAffirmation from './SingleAffirmation'
+import React, { useEffect, useState, useContext } from 'react';
+import './affirmationSection.css';
+import axios from 'axios';
+import SingleAffirmation from './SingleAffirmation';
 import { UserContext } from '../../Context/UserContext';
-export default  function  AffirmationSection() {
 
-  const [ affirmations,setAffirmations] = useState([]);
-  //const [username, setUsername] = useState('');
+const AffirmationSection = () => {
+  const [affirmations, setAffirmations] = useState([]);
+  const [currentAffirmationIndex, setCurrentAffirmationIndex] = useState(0);
+
   const { userValue } = useContext(UserContext);
   const username = userValue.username;
 
-
-useEffect(() => {
-  const fetchAffirmations = async () => {
+  useEffect(() => {
+    const fetchAffirmations = async () => {
       try {
-          if (username) {
-              const response = await axios.get(`http://127.0.0.1:3000/api/affirmations/${username}`);
-              setAffirmations(response.data);
-          }
+        if (username) {
+          const response = await axios.get(`http://127.0.0.1:3000/api/affirmations/${username}`);
+          setAffirmations(response.data);
+          setCurrentAffirmationIndex(0); // Reset current affirmation index when fetching new affirmations
+        }
       } catch (error) {
-          console.error('Error fetching affirmations:', error);
+        console.error('Error fetching affirmations:', error);
       }
-  };
+    };
 
-  fetchAffirmations();
-}, [username]);
+    fetchAffirmations();
+  }, [username]);
+
+  const getNextAffirmation = () => {
+    const newIndex = Math.floor(Math.random() * affirmations.length);
+    setCurrentAffirmationIndex(newIndex);
+  };
 
   return (
     <div className='affirmation-section'>
-    <h3>Your Affirmations 🥠</h3>
-    {affirmations.length > 0 ? (
-        affirmations.map((affirmation) => (
+      <h3>Your Affirmations 🥠</h3>
+      {affirmations.length > 0 ? (
+        <div>
+          {currentAffirmationIndex !== null ? (
             <SingleAffirmation
-             key={affirmation._id}
-              affirmation={affirmation.affirmation}
-              time = {affirmation.updatedAt}
-              id = {affirmation._id}
-               />
-        ))
-    ) : (
+              key={affirmations[currentAffirmationIndex]._id}
+              affirmation={affirmations[currentAffirmationIndex].affirmation}
+              time={affirmations[currentAffirmationIndex].updatedAt}
+              id={affirmations[currentAffirmationIndex]._id}
+              getNextAffirmation = {getNextAffirmation}
+            />
+
+          ) : (
+            <p className='prompt'>Click "Next" to view an affirmation</p>
+          )}
+        </div>
+      ) : (
         <p>Add new affirmation</p>
-    )}
-</div>
+      )}
+        <p className='prompt'>Click "Next" to view an affirmation</p>
+
+    </div>
   );
-}
+};
 
-
+export default AffirmationSection;
--- client/src/components/affirmation/SingleAffirmation.jsx
+++ client/src/components/affirmation/SingleAffirmation.jsx
@@ -4,7 +4,7 @@ import './affirmationSection.css';
 import {useEffect,useContext} from 'react';
 import { UserContext } from '../../Context/UserContext';
 
-function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
+function SingleAffirmation({ affirmation, time, id, getNextAffirmation }) {
   const [isEditing, setIsEditing] = useState(false);
   const [editedAffirmation, setEditedAffirmation] = useState(affirmation);
   const { userValue } = useContext(UserContext);
@@ -100,7 +100,7 @@ function SingleAffirmation({ affirmation, time, id, onAffirmationUpdated }) {
           className='delete-icon'
           onClick={handleDelete}
            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>
-          <button className='next-btn'>next</button>
+          <button  onClick={getNextAffirmation} className='next-btn'>next</button>
         </div>
       </div>
     </>
